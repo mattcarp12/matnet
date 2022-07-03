d95826be098fabebee4687874e1cdc1582703248
Matt Carpenter <mattcarp88@gmail.com>
2022-07-03 12:16:03 -0400

    update example code folder structure

---
+++ examples/tcp_client/tcp_client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"net"
+
+	s "github.com/mattcarp12/go-net/api"
+)
+
+func main() {
+
+	// create new socket
+	sock, err := s.Socket(s.SOCK_STREAM)
+	if err != nil {
+		panic(err)
+	}
+
+	// Make destination address
+	destAddr := flag.String("dest", "", "Destination address")
+	ip := net.ParseIP(*destAddr)
+	if ip == nil {
+		panic("Invalid IP address")
+	}
+
+	dest := s.SockAddr{
+		Port: 8845,
+		IP:   ip,
+	}
+
+	// Connect to the socket
+	if err = s.Connect(sock, dest); err != nil {
+		panic(err)
+	}
+
+	// Write to the socket
+	if err = s.Write(sock, []byte("Hello World\n"), 0); err != nil {
+		panic(err)
+	}
+}
